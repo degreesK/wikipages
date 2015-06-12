2f36f976768fefb998a2d02128146aafe44ea6de
Tania Kelvin <tkdegrees@gmail.com>
2015-06-12 16:51:51 -0700

    first edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   match('contacts', {:via => :get, :to => 'contacts#index'})
+  match('contacts', {:via => :post, :to => 'contacts#create'})
+  match('contacts/:id', {:via => :get, :to => 'contacts#show'})
+  match('contacts/:id', {:via => [:patch, :put], :to => 'contacts#update'})
+  match('contacts/:id', {:via => :delete, :to => 'contacts#destroy'})
+  match('contacts/:id/edit', {:via => :get, :to => 'contacts#edit'})
+  match('/', {:via => :get, :to => 'contacts#index'})
 end
 
 
